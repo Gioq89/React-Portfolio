5a18e052d99558f9fe62bfd73e8348d3c80b3e5d
giomendes <gioq89@gmail.com>
2023-09-14 15:12:10 -0700

    updated app and index files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./index.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="portfolio">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
