cddc8197677ec70dd784b67aee3158eb2cfb1fe6
giomendes <gioq89@gmail.com>
2023-09-14 21:43:14 -0700

    fixed more files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import "./index.css";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
